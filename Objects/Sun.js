f849dd9e6fdc658bbfa3c6f53b5af581a20fb593
Alessandra22 <alessandra.cosenza21@gmail.com>
2022-08-17 18:55:27 +0200

    :pencil2: Translated comments and name in Sun from Italian to English

--- Objects/Sun.js
+++ Objects/Sun.js
@@ -1,7 +1,7 @@
 export class Sun{
     constructor(path) {
-        this.name = "Sole"
-        this.revolution = {x:0, y:0, z:0}   // il sole spinna!
+        this.name = "Sun"
+        this.revolution = {x:0, y:0, z:0}   // the sun spins!
         this.rotation = {x:0, y:0, z:0}
         this.position = {x:0, y:0, z:0}
         this.coords = this.position
